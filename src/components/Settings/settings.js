2cc2ba3437b1dbe9ed80cfe1bfe1ad406376004d
Kegen Guyll <guyllkegen@gmail.com>
2019-03-12 20:14:06 -0500

    router

---
+++ src/components/Settings/settings.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Settings extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h1>Settings</h1>;
+  }
+}
+
+export default Settings;
