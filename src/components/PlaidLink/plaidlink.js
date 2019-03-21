ee27db9792b450cfee36de3f336aaf10190b9fc3
Kegen Guyll <guyllkegen@gmail.com>
2019-03-20 22:42:12 -0500

    added the plaidlink

--- src/components/PlaidLink/plaidlink.js
+++ src/components/PlaidLink/plaidlink.js
@@ -1,20 +1,56 @@
 import React, { Component } from "react";
 import PlaidLink from "react-plaid-link";
+import axios from "axios";
+import db from "../Configuration/firebase";
+import firebase from "firebase";
+import "./plaidlinkStyle.css";
+import {
+  Card,
+  CardContent,
+  Grid,
+  CardActions,
+  Avatar
+} from "@material-ui/core";
 
 class Plaid extends Component {
+  constructor(props) {
+    super(props);
+    this.handleOnSuccess = this.handleOnSuccess.bind(this);
+    const user = firebase.auth().currentUser;
+    this.state = {
+      access_token: null,
+      uid: user.uid
+    };
+  }
   handleOnSuccess(token, metadata) {
-    fetch(
-      "https://us-central1-tapbanking.cloudfunctions.net/PlaidAPI/get_access_token",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(token)
-      }
-    )
-      .then(response => {
-        console.log(response);
+    axios
+      .post(
+        "https://us-central1-tapbanking.cloudfunctions.net/PlaidAPI/get_access_token",
+        { public_token: token }
+      )
+      .then(res => {
+        this.setState(() => ({
+          access_token: res.data.access_token
+        }));
+      })
+      .then(() => {
+        const docRef = db.collection("users").doc(this.state.uid);
+        docRef
+          .get()
+          .then(doc => {
+            if (doc.exists) {
+              docRef.update({
+                access_token: this.state.access_token
+              });
+            } else {
+              docRef.set({
+                access_token: this.state.access_token
+              });
+            }
+          })
+          .then(() => {
+            window.location.reload();
+          });
       })
       .catch(error => {
         console.log(error);
@@ -25,17 +61,32 @@ class Plaid extends Component {
     //handle the case whn you user exits link
   }
   render() {
+    console.log(this.props.history);
     return (
-      <PlaidLink
-        clientName="TapBanking"
-        env="sandbox"
-        product={["auth", "transactions"]}
-        publicKey="d6fed0482ed18248ae2e4380d924fd"
-        onExit={this.handleOnExit}
-        onSuccess={this.handleOnSuccess}
-      >
-        Open Link and connect your bank!
-      </PlaidLink>
+      <div className="plaidLinkBackground">
+        <Grid container justify="center">
+          <Card>
+            <CardContent>
+              <Avatar
+                src="https://img.icons8.com/nolan/100/000000/bank-building.png"
+                style={{ height: "100px", width: "100px" }}
+              />
+            </CardContent>
+            <CardActions>
+              <PlaidLink
+                clientName="TapBanking"
+                env="sandbox"
+                product={["auth", "transactions"]}
+                publicKey="d6fed0482ed18248ae2e4380d924fd"
+                onExit={this.handleOnExit}
+                onSuccess={this.handleOnSuccess}
+              >
+                <p>Connect to Bank</p>
+              </PlaidLink>
+            </CardActions>
+          </Card>
+        </Grid>
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -6,9 +6,8 @@ import * as serviceWorker from "./serviceWorker";
 import Login from "./components/Login/login";
 import Plaid from "./components/PlaidLink/plaidlink";
 import firebase from "firebase";
+import { BrowserRouter as Router } from "react-router-dom";
 import db from "./components/Configuration/firebase";
-
-let hasToken = false;
 
 firebase.auth().onAuthStateChanged(user => {
   if (user) {
@@ -19,21 +18,18 @@ firebase.auth().onAuthStateChanged(user => {
         const data = doc.data();
         if (data.access_token) {
           console.log("Has Access token");
-          hasToken = true;
+          ReactDOM.render(<App user={user} />, document.getElementById("root"));
+          return;
         } else {
           console.log("Doesn't have Access token");
-          hasToken = false;
+          ReactDOM.render(<Plaid />, document.getElementById("root"));
         }
+      } else {
+        ReactDOM.render(<Plaid />, document.getElementById("root"));
       }
     });
   } else {
     ReactDOM.render(<Login />, document.getElementById("root"));
-  }
-
-  if (hasToken === true) {
-    ReactDOM.render(<App user={user} />, document.getElementById("root"));
-  } else {
-    ReactDOM.render(<Plaid />, document.getElementById("root"));
   }
 });
 
