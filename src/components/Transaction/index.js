344a6e0116e6598054a149fb65713059736387e5
Stucki <loganstucki@gmail.com>
2019-03-25 13:57:25 -0500

    initial work on transaction page

--- src/components/Transaction/index.js
+++ src/components/Transaction/index.js
@@ -1,11 +1,119 @@
-import React from "react";
+import React, { Component } from "react";
+import {
+  Grid,
+  CircularProgress,
+  List,
+  ListItem,
+  ListItemText,
+  Divider
+} from "@material-ui/core";
+import "./style.css";
+import axois from "axios";
+import firebase from "firebase";
+import db from "../Configs/firebase";
+import { toast } from "react-toastify";
+import { withRouter } from "react-router-dom";
 
-const Transaction = () => {
-  return (
-    <div>
-      <h1>Transaction</h1>
-    </div>
-  );
-};
+class Transaction extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      access_token: null,
+      identity: null
+    };
+  }
 
-export default Transaction;
+  componentDidMount = () => {
+    const user = firebase.auth().currentUser;
+    const uid = user.uid;
+    const docRef = db.collection("users").doc(uid);
+
+    docRef
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          const data = doc.data();
+          if (data.access_token) {
+            this.setState(() => ({ access_token: data.access_token }));
+          } else {
+            toast(
+              <div style={{ color: "black" }} onClick={this.redirect}>
+                You have not logged in with your bank. <br />{" "}
+                <strong>
+                  Click to add{" "}
+                  <span role="img" aria-label="bank">
+                    🏦
+                  </span>
+                </strong>
+              </div>,
+              {
+                toastId: "mainToast",
+                position: "bottom-center",
+                autoClose: false,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true
+              }
+            );
+          }
+        }
+      })
+      .then(() => {
+        axois
+          .post(
+            "https://us-central1-tapbanking.cloudfunctions.net/PlaidAPI/transactions",
+            {
+              access_token: this.state.access_token,
+              startDate: "2018-01-01",
+              endDate: "2019-01-01"
+            }
+          )
+          .then(res => {
+            this.setState({ transactionsData: res.data.transactions });
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      });
+  };
+
+  redirect = () => {
+    this.props.history.push("/plaidlink");
+  };
+
+  numberWithCommas = x => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
+
+  render() {
+    return (
+      <div>
+        <Grid>
+          {this.state.transactionsData ? (
+            <List>
+              {this.state.transactionsData.transactions.map(transaction => (
+                <div key={transaction.transaction_id}>
+                  <ListItem button>
+                    <ListItemText
+                      primary={`${transaction.amount}${
+                        transaction.iso_currency_code
+                      } to ${transaction.name}`}
+                    />
+                  </ListItem>
+                  <Divider />
+                </div>
+              ))}
+            </List>
+          ) : (
+            <Grid style={{ textAlign: "center" }} item xs={12}>
+              <CircularProgress />
+            </Grid>
+          )}
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Transaction);
