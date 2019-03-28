21b6dfd38b2eb2dc3c6b9c1b5386515ce606e5bf
Stucki <loganstucki@gmail.com>
2019-03-28 07:59:56 -0500

    update transaction api with count and offset

--- src/configs/api.js
+++ src/configs/api.js
@@ -9,32 +9,33 @@ import {
 import db from "./firebase";
 
 const currentDate = new Date();
+let offset = 0;
+let total_transactions = 0;
 
-export const pullVitalData = toastCB => {
+export const pullVitalData = async toastCB => {
   const { access_token } = store.getState();
   const { user } = store.getState();
   const uid = user.uid;
   const docRef = db.collection("users").doc(uid);
   if (!access_token) {
-    return docRef
-      .get()
-      .then(doc => {
-        if (doc.exists) {
-          const data = doc.data();
-          if (data.access_token) {
-            store.dispatch(setAccessToken(data.access_token));
-          } else if (data.dontSkip) {
-            toastCB();
-          }
-        } else {
-          docRef.set({
-            access_token: null,
-            dontSkip: true
-          });
-          toastCB();
-        }
-      })
-      .then(() => pullInitialTransactions());
+    const doc = await docRef.get();
+    if (doc.exists) {
+      const data = doc.data();
+      if (data.access_token) {
+        store.dispatch(setAccessToken(data.access_token));
+        pullInitialTransactions();
+      } else if (data.dontSkip) {
+        toastCB();
+      }
+    } else {
+      docRef.set({
+        access_token: null,
+        dontSkip: true
+      });
+      toastCB();
+    }
+  } else {
+    pullInitialTransactions();
   }
 };
 
@@ -47,16 +48,14 @@ export const pullInitialTransactions = () => {
         "https://us-central1-tapbanking.cloudfunctions.net/PlaidAPI/transactions",
         {
           access_token: access_token,
-          startDate: new Date(
-            new Date().setFullYear(currentDate.getFullYear() - 1)
-          )
-            .toISOString()
-            .split("T")[0],
+          startDate: "1970-01-01",
           endDate: currentDate.toISOString().split("T")[0]
         }
       )
       .then(res => {
         let { transactions, accounts } = res.data.transactions;
+
+        total_transactions = res.data.transactions.total_transactions;
 
         store.dispatch(setTransactions(transactions));
         store.dispatch(setAccounts(accounts));
@@ -67,28 +66,20 @@ export const pullInitialTransactions = () => {
   }
 };
 
-export const pullMoreTransactions = () => {
-  const { transactions, access_token } = store.getState();
+export const pullMoreTransactions = async () => {
+  const { access_token } = store.getState();
 
-  let lastDate = new Date(transactions[transactions.length - 1].date);
-  let newDate = new Date();
-  newDate.setFullYear(lastDate.getFullYear() - 1);
-  newDate = newDate.toISOString().split("T")[0];
-  lastDate.setDate(lastDate.getDay() - 1);
-  lastDate = lastDate.toISOString().split("T")[0];
-  return axois
-    .post(
+  if (offset !== total_transactions) {
+    const res = await axois.post(
       "https://us-central1-tapbanking.cloudfunctions.net/PlaidAPI/transactions",
       {
         access_token,
-        startDate: newDate,
-        endDate: lastDate
+        startDate: "1970-01-01",
+        endDate: currentDate.toISOString().split("T")[0],
+        offset
       }
-    )
-    .then(res => {
-      store.dispatch(addTransactions(res.data.transactions.transactions));
-    })
-    .catch(err => {
-      console.log(err);
-    });
+    );
+    store.dispatch(addTransactions(res.data.transactions.transactions));
+    offset += res.data.transactions.transactions.length;
+  }
 };
